d346099905f9b566ba9ac033a0ce9e6105dea321
lenloe1 <31710001+lenloe1@users.noreply.github.com>
2020-04-10 16:12:50 -0400

    Update af-gen-event.h

--- af-gen-event.h
+++ af-gen-event.h
@@ -11,29 +11,26 @@
 #define EMBER_AF_GENERATED_EVENT_CODE \
   EmberEventControl emberAfIdentifyClusterServerTickCallbackControl1; \
   EmberEventControl emberAfPriceClusterClientTickCallbackControl1; \
+  EmberEventControl emberAfKeyEstablishmentClusterServerTickCallbackControl1; \
   extern EmberEventControl emberAfPluginEzmodeCommissioningStateEventControl; \
   extern EmberEventControl emberAfPluginFormAndJoinCleanupEventControl; \
   extern EmberEventControl emberAfPluginIdentifyFeedbackProvideFeedbackEventControl; \
+  extern EmberEventControl emberAfPluginKeyEstablishmentApsDuplicateDetectionEventControl; \
   extern EmberEventControl emberAfPluginNetworkCreatorSecurityOpenNetworkEventControl; \
   extern EmberEventControl emberAfPluginNetworkFindTickEventControl; \
   extern EmberEventControl emberAfPluginNetworkSteeringFinishSteeringEventControl; \
   extern EmberEventControl emberAfPluginReportingTickEventControl; \
   extern EmberEventControl emberAfPluginScanDispatchScanEventControl; \
-  extern EmberEventControl emberAfPluginTrustCenterNwkKeyUpdateBroadcastMyEventControl; \
-  extern EmberEventControl emberAfPluginTrustCenterNwkKeyUpdatePeriodicMyEventControl; \
-  extern EmberEventControl emberAfPluginTrustCenterNwkKeyUpdateUnicastMyEventControl; \
   extern EmberEventControl emberAfPluginUpdateTcLinkKeyBeginTcLinkKeyUpdateEventControl; \
   extern void emberAfPluginEzmodeCommissioningStateEventHandler(void); \
   extern void emberAfPluginFormAndJoinCleanupEventHandler(void); \
   extern void emberAfPluginIdentifyFeedbackProvideFeedbackEventHandler(void); \
+  extern void emberAfPluginKeyEstablishmentApsDuplicateDetectionEventHandler(void); \
   extern void emberAfPluginNetworkCreatorSecurityOpenNetworkEventHandler(void); \
   extern void emberAfPluginNetworkFindTickEventHandler(void); \
   extern void emberAfPluginNetworkSteeringFinishSteeringEventHandler(void); \
   extern void emberAfPluginReportingTickEventHandler(void); \
   extern void emberAfPluginScanDispatchScanEventHandler(void); \
-  extern void emberAfPluginTrustCenterNwkKeyUpdateBroadcastMyEventHandler(void); \
-  extern void emberAfPluginTrustCenterNwkKeyUpdatePeriodicMyEventHandler(void); \
-  extern void emberAfPluginTrustCenterNwkKeyUpdateUnicastMyEventHandler(void); \
   extern void emberAfPluginUpdateTcLinkKeyBeginTcLinkKeyUpdateEventHandler(void); \
   static void networkEventWrapper(EmberEventControl *control, EmberAfNetworkEventHandler handler, uint8_t networkIndex) \
   { \
@@ -51,9 +48,6 @@
   EmberEventControl emberAfPluginSmartEnergyRegistrationTickNetworkEventControls[1]; \
   extern void emberAfPluginSmartEnergyRegistrationTickNetworkEventHandler(void); \
   void emberAfPluginSmartEnergyRegistrationTickNetworkEventWrapper0(void) { networkEventWrapper(&emberAfPluginSmartEnergyRegistrationTickNetworkEventControls[0], emberAfPluginSmartEnergyRegistrationTickNetworkEventHandler, 0); } \
-  EmberEventControl emberAfPluginTrustCenterKeepaliveTickNetworkEventControls[1]; \
-  extern void emberAfPluginTrustCenterKeepaliveTickNetworkEventHandler(void); \
-  void emberAfPluginTrustCenterKeepaliveTickNetworkEventWrapper0(void) { networkEventWrapper(&emberAfPluginTrustCenterKeepaliveTickNetworkEventControls[0], emberAfPluginTrustCenterKeepaliveTickNetworkEventHandler, 0); } \
   static void clusterTickWrapper(EmberEventControl *control, EmberAfTickFunction callback, uint8_t endpoint) \
   { \
     emberAfPushEndpointNetworkIndex(endpoint); \
@@ -63,57 +57,55 @@
   } \
   void emberAfIdentifyClusterServerTickCallbackWrapperFunction1(void) { clusterTickWrapper(&emberAfIdentifyClusterServerTickCallbackControl1, emberAfIdentifyClusterServerTickCallback, 1); } \
   void emberAfPriceClusterClientTickCallbackWrapperFunction1(void) { clusterTickWrapper(&emberAfPriceClusterClientTickCallbackControl1, emberAfPriceClusterClientTickCallback, 1); } \
+  void emberAfKeyEstablishmentClusterServerTickCallbackWrapperFunction1(void) { clusterTickWrapper(&emberAfKeyEstablishmentClusterServerTickCallbackControl1, emberAfKeyEstablishmentClusterServerTickCallback, 1); } \
 
 
 // EmberEventData structs used to populate the EmberEventData table
 #define EMBER_AF_GENERATED_EVENTS   \
   { &emberAfIdentifyClusterServerTickCallbackControl1, emberAfIdentifyClusterServerTickCallbackWrapperFunction1 }, \
   { &emberAfPriceClusterClientTickCallbackControl1, emberAfPriceClusterClientTickCallbackWrapperFunction1 }, \
+  { &emberAfKeyEstablishmentClusterServerTickCallbackControl1, emberAfKeyEstablishmentClusterServerTickCallbackWrapperFunction1 }, \
   { &emberAfPluginEzmodeCommissioningStateEventControl, emberAfPluginEzmodeCommissioningStateEventHandler }, \
   { &emberAfPluginFormAndJoinCleanupEventControl, emberAfPluginFormAndJoinCleanupEventHandler }, \
   { &emberAfPluginIdentifyFeedbackProvideFeedbackEventControl, emberAfPluginIdentifyFeedbackProvideFeedbackEventHandler }, \
+  { &emberAfPluginKeyEstablishmentApsDuplicateDetectionEventControl, emberAfPluginKeyEstablishmentApsDuplicateDetectionEventHandler }, \
   { &emberAfPluginNetworkCreatorSecurityOpenNetworkEventControl, emberAfPluginNetworkCreatorSecurityOpenNetworkEventHandler }, \
   { &emberAfPluginNetworkFindTickEventControl, emberAfPluginNetworkFindTickEventHandler }, \
   { &emberAfPluginNetworkSteeringFinishSteeringEventControl, emberAfPluginNetworkSteeringFinishSteeringEventHandler }, \
   { &emberAfPluginReportingTickEventControl, emberAfPluginReportingTickEventHandler }, \
   { &emberAfPluginScanDispatchScanEventControl, emberAfPluginScanDispatchScanEventHandler }, \
-  { &emberAfPluginTrustCenterNwkKeyUpdateBroadcastMyEventControl, emberAfPluginTrustCenterNwkKeyUpdateBroadcastMyEventHandler }, \
-  { &emberAfPluginTrustCenterNwkKeyUpdatePeriodicMyEventControl, emberAfPluginTrustCenterNwkKeyUpdatePeriodicMyEventHandler }, \
-  { &emberAfPluginTrustCenterNwkKeyUpdateUnicastMyEventControl, emberAfPluginTrustCenterNwkKeyUpdateUnicastMyEventHandler }, \
   { &emberAfPluginUpdateTcLinkKeyBeginTcLinkKeyUpdateEventControl, emberAfPluginUpdateTcLinkKeyBeginTcLinkKeyUpdateEventHandler }, \
   { &emberAfPluginPartnerLinkKeyExchangeTimeoutNetworkEventControls[0], emberAfPluginPartnerLinkKeyExchangeTimeoutNetworkEventWrapper0 }, \
   { &emberAfPluginPermitJoinManagerTimeoutNetworkEventControls[0], emberAfPluginPermitJoinManagerTimeoutNetworkEventWrapper0 }, \
   { &emberAfPluginSmartEnergyRegistrationTickNetworkEventControls[0], emberAfPluginSmartEnergyRegistrationTickNetworkEventWrapper0 }, \
-  { &emberAfPluginTrustCenterKeepaliveTickNetworkEventControls[0], emberAfPluginTrustCenterKeepaliveTickNetworkEventWrapper0 }, \
 
 
 #define EMBER_AF_GENERATED_EVENT_STRINGS   \
   "Identify Cluster Server EP 1",  \
   "Price Cluster Client EP 1",  \
+  "Key Establishment Cluster Server EP 1",  \
   "EZ-Mode Commissioning Plugin State",  \
   "Form and Join Library Plugin Cleanup",  \
   "Identify Feedback Plugin ProvideFeedback",  \
+  "Key Establishment Plugin ApsDuplicateDetection",  \
   "Network Creator Security Plugin OpenNetwork",  \
   "Network Find Plugin Tick",  \
   "Network Steering Plugin FinishSteering",  \
   "Reporting Plugin Tick",  \
   "Scan Dispatch Plugin Scan",  \
-  "Trust Center Network Key Update Broadcast Plugin My",  \
-  "Trust Center Network Key Update Periodic Plugin My",  \
-  "Trust Center Network Key Update Unicast Plugin My",  \
   "Update TC Link Key Plugin BeginTcLinkKeyUpdate",  \
   "Partner Link Key Exchange Plugin Timeout NWK 0", \
   "Permit Join Manager Plugin Timeout NWK 0", \
   "Smart Energy Registration Plugin Tick NWK 0", \
-  "Trust Center Keepalive Plugin Tick NWK 0", \
 
 
 // The length of the event context table used to track and retrieve cluster events
-#define EMBER_AF_EVENT_CONTEXT_LENGTH 2
+#define EMBER_AF_EVENT_CONTEXT_LENGTH 3
 
 // EmberAfEventContext structs used to populate the EmberAfEventContext table
 #define EMBER_AF_GENERATED_EVENT_CONTEXT { 0x1, 0x3, false, EMBER_AF_LONG_POLL, EMBER_AF_OK_TO_SLEEP, &emberAfIdentifyClusterServerTickCallbackControl1}, \
-{ 0x1, 0x700, true, EMBER_AF_LONG_POLL, EMBER_AF_OK_TO_SLEEP, &emberAfPriceClusterClientTickCallbackControl1}
+{ 0x1, 0x700, true, EMBER_AF_LONG_POLL, EMBER_AF_OK_TO_SLEEP, &emberAfPriceClusterClientTickCallbackControl1}, \
+{ 0x1, 0x800, false, EMBER_AF_LONG_POLL, EMBER_AF_OK_TO_SLEEP, &emberAfKeyEstablishmentClusterServerTickCallbackControl1}
 
 
 #endif // __AF_GEN_EVENT__
