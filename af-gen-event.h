cbb22ace140081e8b4731cd2d7e4b0dbf751233c
lenloe1 <31710001+lenloe1@users.noreply.github.com>
2020-04-10 16:37:31 -0400

    Update af-gen-event.h

--- af-gen-event.h
+++ af-gen-event.h
@@ -11,11 +11,9 @@
 #define EMBER_AF_GENERATED_EVENT_CODE \
   EmberEventControl emberAfIdentifyClusterServerTickCallbackControl1; \
   EmberEventControl emberAfPriceClusterClientTickCallbackControl1; \
-  EmberEventControl emberAfKeyEstablishmentClusterServerTickCallbackControl1; \
   extern EmberEventControl emberAfPluginEzmodeCommissioningStateEventControl; \
   extern EmberEventControl emberAfPluginFormAndJoinCleanupEventControl; \
   extern EmberEventControl emberAfPluginIdentifyFeedbackProvideFeedbackEventControl; \
-  extern EmberEventControl emberAfPluginKeyEstablishmentApsDuplicateDetectionEventControl; \
   extern EmberEventControl emberAfPluginNetworkCreatorSecurityOpenNetworkEventControl; \
   extern EmberEventControl emberAfPluginNetworkFindTickEventControl; \
   extern EmberEventControl emberAfPluginNetworkSteeringFinishSteeringEventControl; \
@@ -25,7 +23,6 @@
   extern void emberAfPluginEzmodeCommissioningStateEventHandler(void); \
   extern void emberAfPluginFormAndJoinCleanupEventHandler(void); \
   extern void emberAfPluginIdentifyFeedbackProvideFeedbackEventHandler(void); \
-  extern void emberAfPluginKeyEstablishmentApsDuplicateDetectionEventHandler(void); \
   extern void emberAfPluginNetworkCreatorSecurityOpenNetworkEventHandler(void); \
   extern void emberAfPluginNetworkFindTickEventHandler(void); \
   extern void emberAfPluginNetworkSteeringFinishSteeringEventHandler(void); \
@@ -57,18 +54,15 @@
   } \
   void emberAfIdentifyClusterServerTickCallbackWrapperFunction1(void) { clusterTickWrapper(&emberAfIdentifyClusterServerTickCallbackControl1, emberAfIdentifyClusterServerTickCallback, 1); } \
   void emberAfPriceClusterClientTickCallbackWrapperFunction1(void) { clusterTickWrapper(&emberAfPriceClusterClientTickCallbackControl1, emberAfPriceClusterClientTickCallback, 1); } \
-  void emberAfKeyEstablishmentClusterServerTickCallbackWrapperFunction1(void) { clusterTickWrapper(&emberAfKeyEstablishmentClusterServerTickCallbackControl1, emberAfKeyEstablishmentClusterServerTickCallback, 1); } \
 
 
 // EmberEventData structs used to populate the EmberEventData table
 #define EMBER_AF_GENERATED_EVENTS   \
   { &emberAfIdentifyClusterServerTickCallbackControl1, emberAfIdentifyClusterServerTickCallbackWrapperFunction1 }, \
   { &emberAfPriceClusterClientTickCallbackControl1, emberAfPriceClusterClientTickCallbackWrapperFunction1 }, \
-  { &emberAfKeyEstablishmentClusterServerTickCallbackControl1, emberAfKeyEstablishmentClusterServerTickCallbackWrapperFunction1 }, \
   { &emberAfPluginEzmodeCommissioningStateEventControl, emberAfPluginEzmodeCommissioningStateEventHandler }, \
   { &emberAfPluginFormAndJoinCleanupEventControl, emberAfPluginFormAndJoinCleanupEventHandler }, \
   { &emberAfPluginIdentifyFeedbackProvideFeedbackEventControl, emberAfPluginIdentifyFeedbackProvideFeedbackEventHandler }, \
-  { &emberAfPluginKeyEstablishmentApsDuplicateDetectionEventControl, emberAfPluginKeyEstablishmentApsDuplicateDetectionEventHandler }, \
   { &emberAfPluginNetworkCreatorSecurityOpenNetworkEventControl, emberAfPluginNetworkCreatorSecurityOpenNetworkEventHandler }, \
   { &emberAfPluginNetworkFindTickEventControl, emberAfPluginNetworkFindTickEventHandler }, \
   { &emberAfPluginNetworkSteeringFinishSteeringEventControl, emberAfPluginNetworkSteeringFinishSteeringEventHandler }, \
@@ -83,11 +77,9 @@
 #define EMBER_AF_GENERATED_EVENT_STRINGS   \
   "Identify Cluster Server EP 1",  \
   "Price Cluster Client EP 1",  \
-  "Key Establishment Cluster Server EP 1",  \
   "EZ-Mode Commissioning Plugin State",  \
   "Form and Join Library Plugin Cleanup",  \
   "Identify Feedback Plugin ProvideFeedback",  \
-  "Key Establishment Plugin ApsDuplicateDetection",  \
   "Network Creator Security Plugin OpenNetwork",  \
   "Network Find Plugin Tick",  \
   "Network Steering Plugin FinishSteering",  \
@@ -100,12 +92,11 @@
 
 
 // The length of the event context table used to track and retrieve cluster events
-#define EMBER_AF_EVENT_CONTEXT_LENGTH 3
+#define EMBER_AF_EVENT_CONTEXT_LENGTH 2
 
 // EmberAfEventContext structs used to populate the EmberAfEventContext table
 #define EMBER_AF_GENERATED_EVENT_CONTEXT { 0x1, 0x3, false, EMBER_AF_LONG_POLL, EMBER_AF_OK_TO_SLEEP, &emberAfIdentifyClusterServerTickCallbackControl1}, \
-{ 0x1, 0x700, true, EMBER_AF_LONG_POLL, EMBER_AF_OK_TO_SLEEP, &emberAfPriceClusterClientTickCallbackControl1}, \
-{ 0x1, 0x800, false, EMBER_AF_LONG_POLL, EMBER_AF_OK_TO_SLEEP, &emberAfKeyEstablishmentClusterServerTickCallbackControl1}
+{ 0x1, 0x700, true, EMBER_AF_LONG_POLL, EMBER_AF_OK_TO_SLEEP, &emberAfPriceClusterClientTickCallbackControl1}
 
 
 #endif // __AF_GEN_EVENT__
