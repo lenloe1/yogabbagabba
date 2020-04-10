40b58df253331a8b68a3cfdeb0eaa705041ae4e3
lenloe1 <31710001+lenloe1@users.noreply.github.com>
2020-04-10 15:28:13 -0400

    Update znet-cli.c

--- znet-cli.c
+++ znet-cli.c
@@ -580,6 +580,47 @@ static EmberCommandEntry emberCommandPluginCountersTable[] = {
   emberCommandEntryActionWithDetails("simple-print", emberAfPluginCountersSimplePrintCommand, "", "Print all counter values.", NULL),
   emberCommandEntryTerminator(),
 };
+void emAfPluginDeviceDatabaseAddDummyDevice(void);
+#if defined(EMBER_COMMAND_INTEPRETER_HAS_DESCRIPTION_FIELD)
+static const char * const pluginDeviceDatabaseDeviceAddDummyCommandArguments[] = {
+  "The address of the dummy device to add.",
+  "The number of dummy endpoints to add.",
+  "The number of dummy clusters to add.",
+  NULL
+};
+#endif
+
+
+void emAfPluginDeviceDatabaseErase(void);
+#if defined(EMBER_COMMAND_INTEPRETER_HAS_DESCRIPTION_FIELD)
+static const char * const pluginDeviceDatabaseDeviceEraseCommandArguments[] = {
+  "The address of the device to erase from the database.",
+  NULL
+};
+#endif
+
+
+void emAfPluginDeviceDatabasePrintDevice(void);
+#if defined(EMBER_COMMAND_INTEPRETER_HAS_DESCRIPTION_FIELD)
+static const char * const pluginDeviceDatabaseDevicePrintCommandArguments[] = {
+  "The address of the device to be looked up (little endian)",
+  NULL
+};
+#endif
+
+
+static EmberCommandEntry emberCommandPluginDeviceDatabaseDeviceTable[] = {
+  emberCommandEntryActionWithDetails("add-dummy", emAfPluginDeviceDatabaseAddDummyDevice, "buv", "Add a device with specified EUI64 and a sequential number of clusters  ...", pluginDeviceDatabaseDeviceAddDummyCommandArguments),
+  emberCommandEntryActionWithDetails("erase", emAfPluginDeviceDatabaseErase, "b", "Erase the device with specified EUI64 from the database.", pluginDeviceDatabaseDeviceEraseCommandArguments),
+  emberCommandEntryActionWithDetails("print", emAfPluginDeviceDatabasePrintDevice, "b", "Print all the clusters and endpoints known about the specified device  ...", pluginDeviceDatabaseDevicePrintCommandArguments),
+  emberCommandEntryTerminator(),
+};
+void emAfPluginDeviceDatabasePrintAll(void);
+static EmberCommandEntry emberCommandPluginDeviceDatabaseTable[] = {
+  emberCommandEntrySubMenu("device", emberCommandPluginDeviceDatabaseDeviceTable, ""),
+  emberCommandEntryActionWithDetails("print-all", emAfPluginDeviceDatabasePrintAll, "", "Print all devices in the database.", NULL),
+  emberCommandEntryTerminator(),
+};
 void emberAfPluginEzModeCommissioningClientCommand(void);
 #if defined(EMBER_COMMAND_INTEPRETER_HAS_DESCRIPTION_FIELD)
 static const char * const pluginEzmodeCommissioningClientCommandArguments[] = {
@@ -607,6 +648,33 @@ static EmberCommandEntry emberCommandPluginEzmodeCommissioningTable[] = {
   emberCommandEntryActionWithDetails("server", emberAfPluginEzModeCommissioningServerCommand, "uv", "Start EZ-Mode server commissioning.", pluginEzmodeCommissioningServerCommandArguments),
   emberCommandEntryTerminator(),
 };
+void emAfPluginFragmentationArtificialBlockDropCommand(void);
+#if defined(EMBER_COMMAND_INTEPRETER_HAS_DESCRIPTION_FIELD) && defined(EMBER_TEST)
+static const char * const pluginFragmentationArtificialBlockDropCommandArguments[] = {
+  "The block number to artificially drop to force a retr ...",
+  NULL
+};
+#endif
+
+
+void emAfPluginFragmentationSetWindowSizeCommand(void);
+#if defined(EMBER_COMMAND_INTEPRETER_HAS_DESCRIPTION_FIELD) && defined(EMBER_TEST)
+static const char * const pluginFragmentationSetRxWindowSizeCommandArguments[] = {
+  "The max number of the blocks received before an APS ack is generated.",
+  NULL
+};
+#endif
+
+
+static EmberCommandEntry emberCommandPluginFragmentationTable[] = {
+#if defined(EMBER_TEST)
+  emberCommandEntryActionWithDetails("artificial-block-drop", emAfPluginFragmentationArtificialBlockDropCommand, "u", "Artificially drops the block number defined by the passed argument onl ...", pluginFragmentationArtificialBlockDropCommandArguments),
+#endif //defined(EMBER_TEST)
+#if defined(EMBER_TEST)
+  emberCommandEntryActionWithDetails("set-rx-window-size", emAfPluginFragmentationSetWindowSizeCommand, "u", "Sets the receive window siz ...", pluginFragmentationSetRxWindowSizeCommandArguments),
+#endif //defined(EMBER_TEST)
+  emberCommandEntryTerminator(),
+};
 void emberAfPluginGatewaySupportTimeSyncLocal(void);
 static EmberCommandEntry emberCommandPluginGatewayTable[] = {
   emberCommandEntryActionWithDetails("time-sync-local", emberAfPluginGatewaySupportTimeSyncLocal, "", "This command will sync the ZCL device's local time attribute with the  ...", NULL),
@@ -615,6 +683,31 @@ static EmberCommandEntry emberCommandPluginGatewayTable[] = {
 void emAfPluginIdentifyCliPrint(void);
 static EmberCommandEntry emberCommandPluginIdentifyTable[] = {
   emberCommandEntryActionWithDetails("print", emAfPluginIdentifyCliPrint, "", "Print which endpoints are reporting.", NULL),
+  emberCommandEntryTerminator(),
+};
+void emAfKeyEstablishmentInterpanCommand(void);
+#if defined(EMBER_COMMAND_INTEPRETER_HAS_DESCRIPTION_FIELD)
+static const char * const pluginKeyEstablishmentInterpanCommandArguments[] = {
+  "The PAN ID that the target is located on.",
+  "The target's EUI64 (big endian)",
+  NULL
+};
+#endif
+
+
+void emAfKeyEstablishmentStartCommand(void);
+#if defined(EMBER_COMMAND_INTEPRETER_HAS_DESCRIPTION_FIELD)
+static const char * const pluginKeyEstablishmentStartCommandArguments[] = {
+  "Target node ID.",
+  "Target node's endpoint.",
+  NULL
+};
+#endif
+
+
+static EmberCommandEntry emberCommandPluginKeyEstablishmentTable[] = {
+  emberCommandEntryActionWithDetails("interpan", emAfKeyEstablishmentInterpanCommand, "vb", "Initiate key establishment with the target device over interpan.", pluginKeyEstablishmentInterpanCommandArguments),
+  emberCommandEntryActionWithDetails("start", emAfKeyEstablishmentStartCommand, "vu", "Initiates key establishment with the target node ID.", pluginKeyEstablishmentStartCommandArguments),
   emberCommandEntryTerminator(),
 };
 void emberAfPluginNetworkCreatorFormCommand(void);
@@ -1186,6 +1279,63 @@ static EmberCommandEntry emberCommandPluginStackDiagnosticsTable[] = {
   emberCommandEntryActionWithDetails("route-table", printRouteTable, "", "Prints out the entries in the stack's route table.", NULL),
   emberCommandEntryTerminator(),
 };
+void importClearCommand(void);
+void emAfTcExportCommand(void);
+#if defined(EMBER_COMMAND_INTEPRETER_HAS_DESCRIPTION_FIELD) && defined(EMBER_AF_PLUGIN_TRUST_CENTER_BACKUP_POSIX_FILE_BACKUP_SUPPORT)
+static const char * const pluginTrustCenterBackupFileExportCommandArguments[] = {
+  "path",
+  NULL
+};
+#endif
+
+
+void emAfTcImportCommand(void);
+#if defined(EMBER_COMMAND_INTEPRETER_HAS_DESCRIPTION_FIELD) && defined(EMBER_AF_PLUGIN_TRUST_CENTER_BACKUP_POSIX_FILE_BACKUP_SUPPORT)
+static const char * const pluginTrustCenterBackupFileImportCommandArguments[] = {
+  "path",
+  NULL
+};
+#endif
+
+
+void importKeyCommand(void);
+#if defined(EMBER_COMMAND_INTEPRETER_HAS_DESCRIPTION_FIELD)
+static const char * const pluginTrustCenterBackupImportKeyCommandArguments[] = {
+  "index",
+  "partner e u i64",
+  "new key",
+  NULL
+};
+#endif
+
+
+void printExportDataCommand(void);
+void printImportDataCommand(void);
+void restoreFromBackupCommand(void);
+void setExtendedPanIdCommand(void);
+#if defined(EMBER_COMMAND_INTEPRETER_HAS_DESCRIPTION_FIELD)
+static const char * const pluginTrustCenterBackupSetExtPanCommandArguments[] = {
+  "extended pan id",
+  NULL
+};
+#endif
+
+
+static EmberCommandEntry emberCommandPluginTrustCenterBackupTable[] = {
+  emberCommandEntryActionWithDetails("clear-import", importClearCommand, "", "Clear the import data set.", NULL),
+#if defined(EMBER_AF_PLUGIN_TRUST_CENTER_BACKUP_POSIX_FILE_BACKUP_SUPPORT)
+  emberCommandEntryActionWithDetails("file-export", emAfTcExportCommand, "b", "Write the TC backup data to a file.", pluginTrustCenterBackupFileExportCommandArguments),
+#endif //defined(EMBER_AF_PLUGIN_TRUST_CENTER_BACKUP_POSIX_FILE_BACKUP_SUPPORT)
+#if defined(EMBER_AF_PLUGIN_TRUST_CENTER_BACKUP_POSIX_FILE_BACKUP_SUPPORT)
+  emberCommandEntryActionWithDetails("file-import", emAfTcImportCommand, "b", "Read the TC Backup data from a file and bring the TC back online.", pluginTrustCenterBackupFileImportCommandArguments),
+#endif //defined(EMBER_AF_PLUGIN_TRUST_CENTER_BACKUP_POSIX_FILE_BACKUP_SUPPORT)
+  emberCommandEntryActionWithDetails("import-key", importKeyCommand, "ubb", "Set a key in the import data set.", pluginTrustCenterBackupImportKeyCommandArguments),
+  emberCommandEntryActionWithDetails("print-export", printExportDataCommand, "", "Print the set of export data that a TC must backup.", NULL),
+  emberCommandEntryActionWithDetails("print-import", printImportDataCommand, "", "Print the import data set.", NULL),
+  emberCommandEntryActionWithDetails("restore", restoreFromBackupCommand, "", "Use the import data set in a restore operation to bring the TC back on ...", NULL),
+  emberCommandEntryActionWithDetails("set-ext-pan", setExtendedPanIdCommand, "b", "Set the extended PAN ID in the import data set.", pluginTrustCenterBackupSetExtPanCommandArguments),
+  emberCommandEntryTerminator(),
+};
 void emberAfPluginSetTCLinkKeyUpdateTimerCommand(void);
 #if defined(EMBER_COMMAND_INTEPRETER_HAS_DESCRIPTION_FIELD)
 static const char * const pluginUpdateTcLinkKeyTimerCommandArguments[] = {
@@ -1204,9 +1354,12 @@ static EmberCommandEntry emberCommandPluginTable[] = {
   emberCommandEntrySubMenu("concentrator", emberCommandPluginConcentratorTable, ""),
   emberCommandEntrySubMenu("counter", emberCommandPluginCounterTable, ""),
   emberCommandEntrySubMenu("counters", emberCommandPluginCountersTable, ""),
+  emberCommandEntrySubMenu("device-database", emberCommandPluginDeviceDatabaseTable, ""),
   emberCommandEntrySubMenu("ezmode-commissioning", emberCommandPluginEzmodeCommissioningTable, ""),
+  emberCommandEntrySubMenu("fragmentation", emberCommandPluginFragmentationTable, ""),
   emberCommandEntrySubMenu("gateway", emberCommandPluginGatewayTable, ""),
   emberCommandEntrySubMenu("identify", emberCommandPluginIdentifyTable, ""),
+  emberCommandEntrySubMenu("key-establishment", emberCommandPluginKeyEstablishmentTable, ""),
   emberCommandEntrySubMenu("network-creator", emberCommandPluginNetworkCreatorTable, ""),
   emberCommandEntrySubMenu("network-creator-security", emberCommandPluginNetworkCreatorSecurityTable, ""),
   emberCommandEntrySubMenu("network-steering", emberCommandPluginNetworkSteeringTable, ""),
@@ -1216,6 +1369,7 @@ static EmberCommandEntry emberCommandPluginTable[] = {
   emberCommandEntrySubMenu("simple-metering-client", emberCommandPluginSimpleMeteringClientTable, ""),
   emberCommandEntrySubMenu("smart-energy-registration", emberCommandPluginSmartEnergyRegistrationTable, ""),
   emberCommandEntrySubMenu("stack-diagnostics", emberCommandPluginStackDiagnosticsTable, ""),
+  emberCommandEntrySubMenu("trust-center-backup", emberCommandPluginTrustCenterBackupTable, ""),
   emberCommandEntrySubMenu("update-tc-link-key", emberCommandPluginUpdateTcLinkKeyTable, ""),
   emberCommandEntryTerminator(),
 };
